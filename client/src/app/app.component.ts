cb26823d8cf801fba1d0357a725a14db03cb87a6
Lalo Serna <eserna@grapheke.com>
2023-12-11 17:20:23 -0600

    End of Section 8
    Add product listing and navigation bar to the app
    
    Introduced a navigation bar to the app and created a product listing page. This involved model creation for products with relevant properties and implementation of a service to fetch product data from the API. As part of this update, changes were made in the Visual Studio Code settings and indentation size in the editor config was set to 4.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,14 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {NavBarComponent} from "./nav-bar/nav-bar.component";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {Product} from "./models/product";
+import {Pagination} from "./models/pagination";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    imports: [CommonModule, RouterOutlet, NavBarComponent, HttpClientModule],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'Skinet';
+export class AppComponent implements OnInit{
+    title = 'Skinet';
+    products: Product[] = [];
+
+    constructor(private http: HttpClient) {}
+
+    ngOnInit() {
+        this.http.get<Pagination<Product[]>>('https://localhost:5001/api/products?pageSize=50')
+            .subscribe({
+                next: (res: any) => this.products = res.data,
+                error: err => console.log(err),
+                complete: () => {
+                    console.log('Completed');
+                }
+            })
+    }
+
 }
